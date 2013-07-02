60874da07f5cd14761aa6a3c246466f26167c75b
vGodFather <vGodFather@e0609765-30a6-43e4-aed0-291324df32eb>
2013-07-02 01:59:22 +0000

    Fixed Isle of prayer drops Fixed Quest the one who ends silence

--- L2J_ReunionProject_Scripts/java/l2r/gameserver/scripts/quests/Q00270_TheOneWhoEndsSilence.java
+++ L2J_ReunionProject_Scripts/java/l2r/gameserver/scripts/quests/Q00270_TheOneWhoEndsSilence.java
@@ -128,9 +128,10 @@ public class Q00270_TheOneWhoEndsSilence extends Quest
 					{
 						rewardScroll(st, 1);
 					}
-					htmltext = "32757-09.html";
-					st.playSound(QuestSound.ITEMSOUND_QUEST_MIDDLE);
+					
 					st.takeItems(TATTERED_MONK_CLOTHES, 100);
+					st.playSound(QuestSound.ITEMSOUND_QUEST_MIDDLE);
+					htmltext = "32757-09.html";
 				}
 				else
 				{
@@ -151,9 +152,10 @@ public class Q00270_TheOneWhoEndsSilence extends Quest
 						st.giveItems(10397 + getRandom(9), 1);
 					}
 					rewardScroll(st, 2);
-					htmltext = "32757-09.html";
-					st.playSound(QuestSound.ITEMSOUND_QUEST_MIDDLE);
+					
 					st.takeItems(TATTERED_MONK_CLOTHES, 200);
+					st.playSound(QuestSound.ITEMSOUND_QUEST_MIDDLE);
+					htmltext = "32757-09.html";
 				}
 				else
 				{
@@ -168,9 +170,10 @@ public class Q00270_TheOneWhoEndsSilence extends Quest
 					st.giveItems(10373 + getRandom(9), 1);
 					st.giveItems(10397 + getRandom(9), 1);
 					rewardScroll(st, 3);
-					htmltext = "32757-09.html";
-					st.playSound(QuestSound.ITEMSOUND_QUEST_MIDDLE);
+					
 					st.takeItems(TATTERED_MONK_CLOTHES, 300);
+					st.playSound(QuestSound.ITEMSOUND_QUEST_MIDDLE);
+					htmltext = "32757-09.html";
 				}
 				else
 				{
@@ -201,9 +204,10 @@ public class Q00270_TheOneWhoEndsSilence extends Quest
 					{
 						rewardScroll(st, 1);
 					}
-					htmltext = "32757-09.html";
-					st.playSound(QuestSound.ITEMSOUND_QUEST_MIDDLE);
+					
 					st.takeItems(TATTERED_MONK_CLOTHES, 400);
+					st.playSound(QuestSound.ITEMSOUND_QUEST_MIDDLE);
+					htmltext = "32757-09.html";
 				}
 				else
 				{
@@ -227,10 +231,11 @@ public class Q00270_TheOneWhoEndsSilence extends Quest
 					{
 						st.giveItems(10397 + getRandom(9), 1);
 					}
+					
 					rewardScroll(st, 2);
-					htmltext = "32757-09.html";
+					st.takeItems(TATTERED_MONK_CLOTHES, 500);
 					st.playSound(QuestSound.ITEMSOUND_QUEST_MIDDLE);
-					st.takeItems(TATTERED_MONK_CLOTHES, 300);
+					htmltext = "32757-09.html";
 				}
 				else
 				{
