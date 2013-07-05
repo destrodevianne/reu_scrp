3695ae8d3d4eb1b6d59e99acbdb3b973e35f9a26
vGodFather <vGodFather@e0609765-30a6-43e4-aed0-291324df32eb>
2013-07-05 02:39:38 +0000

    Fixing modifiers

--- trunk/L2J_ReunionProject_Scripts/java/l2r/gameserver/scripts/ai/modifier/NoRandomAnimation.java
+++ trunk/L2J_ReunionProject_Scripts/java/l2r/gameserver/scripts/ai/modifier/NoRandomAnimation.java
@@ -14,105 +14,51 @@
  */
 package l2r.gameserver.scripts.ai.modifier;
 
-import l2r.gameserver.datatables.SpawnTable;
-import l2r.gameserver.model.L2Spawn;
+import l2r.gameserver.model.L2Object;
+import l2r.gameserver.model.L2World;
 import l2r.gameserver.model.actor.L2Npc;
 import l2r.gameserver.scripts.ai.npc.AbstractNpcAI;
-import l2r.gameserver.util.Util;
 
 /**
  * @author Kazumi
  */
 public class NoRandomAnimation extends AbstractNpcAI
 {
+	// @formatter:off
 	private final static int[] NO_ANIMATION_MOBS_LIST =
 	{
-		13148,
-		18635,
-		18636,
-		18638,
-		18639,
-		18640,
-		18641,
-		18642,
-		18644,
-		18645,
-		18646,
-		18648,
-		18649,
-		18650,
-		18652,
-		18653,
-		18654,
-		18655,
-		18656,
-		18657,
-		18658,
-		18659,
-		18660,
-		18704,
-		18705,
-		18706,
-		18708,
-		18709,
-		18710,
-		18711,
-		18805,
-		18806,
-		18811,
-		22136,
-		29045,
-		29046,
-		29047,
-		29048,
-		29049,
-		29050,
-		29051,
-		29099,
-		29103,
-		29150,
-		29151,
-		29152,
-		29161,
-		29163,
-		29173,
-		29174,
-		29175,
-		30675,
-		30761,
-		30762,
-		30763,
-		30980,
-		31074,
-		31665,
-		32746,
-		31752,
-		32015,
-		32568,
-		32556,
-		32568
+		13148, 18635, 18636, 18638, 18639, 18640, 18641, 18642, 18644, 18645, 18646, 18648,
+		18649, 18650, 18652, 18653, 18654, 18655, 18656, 18657, 18658, 18659, 18660, 18704,
+		18705, 18706, 18708, 18709, 18710, 18711, 18805, 18806, 18811, 22136, 29045, 29046,
+		29047, 29048, 29049, 29050, 29051, 29099, 29103, 29150, 29151, 29152, 29161, 29163,
+		29173, 29174, 29175, 30675, 30761, 30762, 30763, 30980, 31074, 31665, 32746, 31752,
+		32015, 32568, 32556, 32568,22786,22787,
+		22839, 22841 //LoA client not have social
 	};
+	// @formatter:on
 	
 	public NoRandomAnimation(int questId, String name, String descr)
 	{
 		super(name, descr);
-		addSpawnId(NO_ANIMATION_MOBS_LIST);
 		
-		for (int npcId : NO_ANIMATION_MOBS_LIST)
+		for (L2Object obj : L2World.getInstance().getAllVisibleObjectsArray())
 		{
-			for (L2Spawn spawn : SpawnTable.getInstance().getSpawns(npcId))
+			if ((obj instanceof L2Npc) && contains(NO_ANIMATION_MOBS_LIST, ((L2Npc) obj).getNpcId()))
 			{
-				if (spawn.getLastSpawn() != null)
-					spawn.getLastSpawn().setRandomAnimationEnabled(false);
+				((L2Npc) obj).setRandomAnimationEnabled(false);
 			}
 		}
+		
+		registerMobs(NO_ANIMATION_MOBS_LIST, QuestEventType.ON_SPAWN);
 	}
 	
 	@Override
 	public String onSpawn(L2Npc npc)
 	{
-		if (Util.contains(NO_ANIMATION_MOBS_LIST, npc.getNpcId()))
+		if (contains(NO_ANIMATION_MOBS_LIST, npc.getNpcId()))
+		{
 			npc.setRandomAnimationEnabled(false);
+		}
 		
 		return super.onSpawn(npc);
 	}
