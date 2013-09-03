590cf8ff19e4723742dccb5bf6e6f4e003f64d36
vGodFather <vGodFather@e0609765-30a6-43e4-aed0-291324df32eb>
2013-09-03 21:51:45 +0000

    Quest fixes Quests In java: Q00106 Q00230 Q00316 Q00317 Q00320 Q00337 Q00348 Q00421 Q00503

--- trunk/L2J_ReunionProject_Scripts/java/l2r/gameserver/scripts/quests/Q00902_ReclaimOurEra.java
+++ trunk/L2J_ReunionProject_Scripts/java/l2r/gameserver/scripts/quests/Q00902_ReclaimOurEra.java
@@ -1,203 +1,247 @@
+/*
+ * Copyright (C) 2004-2013 L2J DataPack
+ * 
+ * This file is part of L2J DataPack.
+ * 
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package l2r.gameserver.scripts.quests;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import l2r.gameserver.model.actor.L2Npc;
 import l2r.gameserver.model.actor.instance.L2PcInstance;
 import l2r.gameserver.model.quest.Quest;
 import l2r.gameserver.model.quest.QuestState;
+import l2r.gameserver.model.quest.QuestState.QuestType;
 import l2r.gameserver.model.quest.State;
 import l2r.gameserver.util.Util;
 
-public class Q00902_ReclaimOurEra extends Quest
+/**
+ * Reclaim Our Era (902)
+ * @author netvirus
+ */
+public final class Q00902_ReclaimOurEra extends Quest
 {
-	private static final int Mathias = 31340;
-	private static final int[] Ketra_Varka =
+	// Npc
+	private static final int MATHIAS = 31340;
+	// Misc
+	private static final int MIN_LVL = 80;
+	// Items
+	private static final int SHATTERED_BONES = 21997;
+	private static final int CANNIBALISTIC_STAKATO_LDR_CLAW = 21998;
+	private static final int ANAIS_SCROLL = 21999;
+	private static final int PROOF_OF_CHALLENGE = 21750;
+	// Monsters
+	private static final Map<Integer, Integer> MONSTER_DROPS = new HashMap<>();
+	
+	static
 	{
-		25309,
-		25312,
-		25315,
-		25299,
-		25302,
-		25305
-	};
-	private static final int[] Stakato_Nest =
+		MONSTER_DROPS.put(25309, SHATTERED_BONES); // Varka's Hero Shadith
+		MONSTER_DROPS.put(25312, SHATTERED_BONES); // Varka's Commander Mos
+		MONSTER_DROPS.put(25315, SHATTERED_BONES); // Varka's Chief Horus
+		MONSTER_DROPS.put(25299, SHATTERED_BONES); // Ketra's Hero Hekaton
+		MONSTER_DROPS.put(25302, SHATTERED_BONES); // Ketra's Commander Tayr
+		MONSTER_DROPS.put(25305, SHATTERED_BONES); // Ketra's Chief Brakki
+		MONSTER_DROPS.put(25667, CANNIBALISTIC_STAKATO_LDR_CLAW); // Cannibalistic Stakato Chief
+		MONSTER_DROPS.put(25668, CANNIBALISTIC_STAKATO_LDR_CLAW); // Cannibalistic Stakato Chief
+		MONSTER_DROPS.put(25669, CANNIBALISTIC_STAKATO_LDR_CLAW); // Cannibalistic Stakato Chief
+		MONSTER_DROPS.put(25670, CANNIBALISTIC_STAKATO_LDR_CLAW); // Cannibalistic Stakato Chief
+		MONSTER_DROPS.put(25701, ANAIS_SCROLL); // Anais - Master of Splendor
+	}
+	
+	private Q00902_ReclaimOurEra(int questId, String name, String descr)
 	{
-		25667,
-		25668,
-		25669,
-		25670
-	};
-	private static final int Monastery_of_Silence = 29096;
-	private static final int[] boss_for_kills =
+		super(questId, name, descr);
+		addStartNpc(MATHIAS);
+		addTalkId(MATHIAS);
+		addKillId(MONSTER_DROPS.keySet());
+		registerQuestItems(SHATTERED_BONES, CANNIBALISTIC_STAKATO_LDR_CLAW, ANAIS_SCROLL);
+	}
+	
+	private void giveItem(L2Npc npc, L2PcInstance player)
 	{
-		25309,
-		25312,
-		25315,
-		25299,
-		25302,
-		25305,
-		25667,
-		25668,
-		25669,
-		25670,
-		29096
-	};
-	private static final int _Shattered_Bones = 21997;
-	private static final int _Stakato_Leader_Claw = 21998;
-	private static final int _AnaisScroll = 21999;
-	private static final int _Blue_Elmore_Coin = 21750;
+		final QuestState st = player.getQuestState(getName());
+		if ((st != null) && (st.isStarted()) && (!st.isCond(5)) && Util.checkIfInRange(1500, npc, player, false))
+		{
+			st.giveItems(MONSTER_DROPS.get(npc.getNpcId()), 1);
+			st.setCond(5, true);
+		}
+	}
 	
 	@Override
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
-		String htmltext = event;
-		QuestState st = player.getQuestState(getName());
-		
+		final QuestState st = player.getQuestState(getName());
+		String htmltext = null;
 		if (st == null)
-			return htmltext;
-		
-		if (npc.getNpcId() == Mathias)
 		{
-			if (event.equalsIgnoreCase("31340-04.htm"))
+			return htmltext;
+		}
+		
+		switch (event)
+		{
+			case "31340-04.htm":
 			{
-				st.setState(State.STARTED);
-				st.set("cond", "1");
-				st.playSound("ItemSound.quest_accept");
+				if (st.isCreated())
+				{
+					htmltext = event;
+				}
+				break;
 			}
-			else if (event.equalsIgnoreCase("31340-06.htm"))
+			case "31340-05.html":
 			{
-				st.set("cond", "2");
-				st.playSound("ItemSound.quest_middle");
+				if (st.isCreated())
+				{
+					st.startQuest();
+					htmltext = event;
+				}
+				break;
 			}
-			else if (event.equalsIgnoreCase("31340-08.htm"))
+			case "31340-06.html":
 			{
-				st.set("cond", "3");
-				st.playSound("ItemSound.quest_middle");
+				if (st.isCond(1))
+				{
+					st.setCond(2, true);
+					htmltext = event;
+				}
+				break;
 			}
-			else if (event.equalsIgnoreCase("31340-10.htm"))
+			case "31340-07.html":
 			{
-				st.set("cond", "4");
-				st.playSound("ItemSound.quest_middle");
+				if (st.isCond(1))
+				{
+					st.setCond(3, true);
+					htmltext = event;
+				}
+				break;
+			}
+			case "31340-08.html":
+			{
+				if (st.isCond(1))
+				{
+					st.setCond(4, true);
+					htmltext = event;
+				}
+				break;
+			}
+			case "31340-10.html":
+			{
+				if (st.isCond(1))
+				{
+					htmltext = event;
+				}
+				break;
 			}
 		}
 		return htmltext;
+	}
+	
+	@Override
+	public String onKill(L2Npc npc, L2PcInstance killer, boolean isSummon)
+	{
+		if (killer.isInParty())
+		{
+			for (L2PcInstance member : killer.getParty().getMembers())
+			{
+				giveItem(npc, member);
+			}
+		}
+		else
+		{
+			giveItem(npc, killer);
+		}
+		return super.onKill(npc, killer, isSummon);
 	}
 	
 	@Override
 	public String onTalk(L2Npc npc, L2PcInstance player)
 	{
-		String htmltext = getNoQuestMsg(player);
 		QuestState st = player.getQuestState(getName());
+		String htmltext = getNoQuestMsg(player);
 		if (st == null)
-			return htmltext;
-		
-		if (npc.getNpcId() == Mathias)
 		{
-			switch (st.getState())
+			return htmltext;
+		}
+		
+		switch (st.getState())
+		{
+			case State.COMPLETED:
 			{
-				case State.CREATED:
-					if (player.getLevel() >= 80)
-						htmltext = "31340-01.htm";
-					else
-						htmltext = "31340-00.htm";
+				if (!st.isNowAvailable())
+				{
+					htmltext = "31340-02.htm";
 					break;
-				case State.COMPLETED:
-					htmltext = "31340-completed.htm";
-					break;
-				case State.STARTED:
-					if (st.getInt("cond") == 2)
-						htmltext = "31340-07.htm";
-					else if (st.getInt("cond") == 3)
-						htmltext = "31340-09.htm";
-					else if (st.getInt("cond") == 4)
-						htmltext = "31340-11.htm";
-					
-					else if (st.getInt("cond") == 5 && st.getQuestItemsCount(_Shattered_Bones) > 0)
+				}
+				st.setState(State.CREATED);
+			}
+			case State.CREATED:
+			{
+				htmltext = (player.getLevel() >= MIN_LVL) ? "31340-01.htm" : "31340-03.htm";
+				break;
+			}
+			case State.STARTED:
+			{
+				switch (st.getCond())
+				{
+					case 1:
 					{
-						htmltext = "31340-12.htm";
-						st.takeItems(_Shattered_Bones, 1);
-						st.giveItems(_Blue_Elmore_Coin, 1);
-						st.giveItems(57, 134038);
-						st.unset("cond");
-						st.playSound("ItemSound.quest_finish");
-						st.exitQuest(false);
+						htmltext = "31340-09.html";
+						break;
 					}
-					else if (st.getInt("cond") == 5 && st.getQuestItemsCount(_Stakato_Leader_Claw) > 0)
+					case 2:
 					{
-						htmltext = "31340-12.htm";
-						st.takeItems(_Stakato_Leader_Claw, 1);
-						st.giveItems(_Blue_Elmore_Coin, 3);
-						st.giveItems(57, 210119);
-						st.unset("cond");
-						st.playSound("ItemSound.quest_finish");
-						st.exitQuest(false);
+						htmltext = "31340-11.html";
+						break;
 					}
-					else if (st.getInt("cond") == 5 && st.getQuestItemsCount(_AnaisScroll) > 0)
+					case 3:
 					{
-						htmltext = "31340-12.htm";
-						st.takeItems(_AnaisScroll, 1);
-						st.giveItems(_Blue_Elmore_Coin, 3);
-						st.giveItems(57, 348155);
-						st.unset("cond");
-						st.playSound("ItemSound.quest_finish");
-						st.exitQuest(false);
+						htmltext = "31340-12.html";
+						break;
 					}
-					else
-						htmltext = "31340-05.html";
-					break;
+					case 4:
+					{
+						htmltext = "31340-13.html";
+						break;
+					}
+					case 5:
+					{
+						if (st.hasQuestItems(SHATTERED_BONES))
+						{
+							st.giveItems(PROOF_OF_CHALLENGE, 1);
+							st.giveAdena(134038, true);
+						}
+						else if (st.hasQuestItems(CANNIBALISTIC_STAKATO_LDR_CLAW))
+						{
+							st.giveItems(PROOF_OF_CHALLENGE, 3);
+							st.giveAdena(210119, true);
+						}
+						else if (st.hasQuestItems(ANAIS_SCROLL))
+						{
+							st.giveItems(PROOF_OF_CHALLENGE, 3);
+							st.giveAdena(348155, true);
+						}
+						st.exitQuest(QuestType.DAILY, true);
+						htmltext = "31340-14.html";
+						break;
+					}
+				}
+				break;
 			}
 		}
 		return htmltext;
-	}
-	
-	@Override
-	public String onKill(L2Npc npc, L2PcInstance killer, boolean isPet)
-	{
-		if (killer.isInParty())
-		{
-			for (L2PcInstance player : killer.getParty().getMembers())
-				rewardPlayer(npc, player);
-		}
-		else
-			rewardPlayer(npc, killer);
-		return super.onKill(npc, killer, isPet);
-	}
-	
-	private void rewardPlayer(L2Npc npc, L2PcInstance player)
-	{
-		QuestState st = player.getQuestState(getName());
-		if (st != null && st.getState() == State.STARTED && player.isInsideRadius(npc, 2000, false, false))
-		{
-			if (st.getInt("cond") == 2 && Util.contains(Ketra_Varka, npc.getNpcId()))
-			{
-				st.giveItems(_Shattered_Bones, 1);
-				st.playSound("ItemSound.quest_middle");
-				st.set("cond", "5");
-			}
-			else if (st.getInt("cond") == 3 && Util.contains(Stakato_Nest, npc.getNpcId()))
-			{
-				st.giveItems(_Stakato_Leader_Claw, 1);
-				st.playSound("ItemSound.quest_middle");
-				st.set("cond", "5");
-			}
-			else if (st.getInt("cond") == 4 && npc.getNpcId() == Monastery_of_Silence)
-			{
-				st.giveItems(_AnaisScroll, 1);
-				st.playSound("ItemSound.quest_middle");
-				st.set("cond", "5");
-			}
-		}
-	}
-	
-	public Q00902_ReclaimOurEra(int questId, String name, String descr)
-	{
-		super(questId, name, descr);
-		
-		addStartNpc(Mathias);
-		addTalkId(Mathias);
-		for (int _npc : boss_for_kills)
-		{
-			addKillId(_npc);
-		}
 	}
 	
 	public static void main(String[] args)
