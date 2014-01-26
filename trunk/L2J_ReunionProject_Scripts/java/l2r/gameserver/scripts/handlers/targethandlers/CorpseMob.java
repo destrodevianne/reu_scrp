b15f3f2e018cb98d7d4e28bc30e4194ef9181b1e
vGodFather <vGodFather@e0609765-30a6-43e4-aed0-291324df32eb>
2014-01-26 21:15:13 +0000

    Fixed CorpseMob scripts
    Check core commit

--- trunk/L2J_ReunionProject_Scripts/java/l2r/gameserver/scripts/handlers/targethandlers/CorpseMob.java
+++ trunk/L2J_ReunionProject_Scripts/java/l2r/gameserver/scripts/handlers/targethandlers/CorpseMob.java
@@ -18,16 +18,13 @@
  */
 package l2r.gameserver.scripts.handlers.targethandlers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import l2r.Config;
 import l2r.gameserver.handler.ITargetTypeHandler;
 import l2r.gameserver.model.L2Object;
 import l2r.gameserver.model.actor.L2Attackable;
 import l2r.gameserver.model.actor.L2Character;
-import l2r.gameserver.model.actor.instance.L2ServitorInstance;
 import l2r.gameserver.model.skills.L2Skill;
+import l2r.gameserver.model.skills.L2SkillType;
 import l2r.gameserver.model.skills.targets.L2TargetType;
 import l2r.gameserver.network.SystemMessageId;
 
@@ -45,40 +42,16 @@ public class CorpseMob implements ITargetTypeHandler
 			return _emptyTargetList;
 		}
 		
-		List<L2Character> targetList = new ArrayList<>();
-		final boolean isSummon = target.isServitor();
-		if (!isSummon)
+		if ((skill.getSkillType() == L2SkillType.SUMMON) && target.isServitor() && (target.getActingPlayer() != null) && (target.getActingPlayer().getObjectId() == activeChar.getObjectId()))
 		{
-			activeChar.sendPacket(SystemMessageId.TARGET_IS_INCORRECT);
 			return _emptyTargetList;
 		}
 		
-		// Corpse mob only available for half time
-		switch (skill.getSkillType())
+		if ((skill.getSkillType() == L2SkillType.DRAIN) && ((L2Attackable) target).isOldCorpse(activeChar.getActingPlayer(), (Config.NPC_DECAY_TIME / 2), true))
 		{
-			case SUMMON:
-			{
-				if (isSummon && (((L2ServitorInstance) target).getOwner() != null) && (((L2ServitorInstance) target).getOwner().getObjectId() == activeChar.getObjectId()))
-				{
-					return _emptyTargetList;
-				}
-				
-				break;
-			}
-			case DRAIN:
-			{
-				if (((L2Attackable) target).isOldCorpse(activeChar.getActingPlayer(), (Config.NPC_DECAY_TIME / 2), true))
-				{
-					return _emptyTargetList;
-				}
-			}
+			return _emptyTargetList;
 		}
 		
-		if (!onlyFirst)
-		{
-			targetList.add(target);
-			return targetList.toArray(new L2Object[targetList.size()]);
-		}
 		return new L2Character[]
 		{
 			target
